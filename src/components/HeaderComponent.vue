e1d9c67f0d887c1af311ad7fb3664d9607e3e7de
Davide Pulci <davide.pulci@gmail.com>
2022-04-26 14:20:16 +0200

    aggiunte modifiche all'header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col">
-        <img src="@/assets/images/logo.png" alt="" />
+      <div class="col-2 offset-1">
+        <img src="@/assets/images/logo.png" alt="" class="w_200" />
       </div>
-      <div class="col-6 d-flex align-items-center">
+      <div class="col-5 d-flex align-items-center">
         <ul class="d-flex w-100 justify-content-around mb-0">
           <li v-for="item in menuItem" :key="item.message">
             <a href="#">{{ item.message }}</a>
           </li>
         </ul>
       </div>
-      <div class="col">
+      <div class="col-3">
         <div class="d-flex justify-content-end align-items-center">
           <button class="btn btn_blue px-4">Schedule a workout</button>
           <i class="fa-solid fa-cart-shopping mx-4"></i>
@@ -37,6 +37,7 @@ ul {
 
   li {
     list-style-type: none;
+    font-size: 16px;
 
     a {
       text-decoration: none;
@@ -49,9 +50,18 @@ ul {
   }
 }
 
+button {
+  font-size: 16px;
+}
+
+// utilities
 .btn_blue {
   background-color: #4154fe;
   color: white;
   border-radius: 50px;
+}
+
+.w_200 {
+  width: 200px;
 }
 </style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container-fluid">
+  <div class="container">
     <div class="row"></div>
   </div>
 </template>
