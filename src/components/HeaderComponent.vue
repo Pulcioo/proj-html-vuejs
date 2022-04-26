64d4bebcc612b2da4757d54ea5da99d079045232
Davide Pulci <davide.pulci@gmail.com>
2022-04-26 12:30:22 +0200

    aggiunti macrocomponenti

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container-fluid">
+    <div class="row"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+};
+</script>
+
+<style>
+</style>
